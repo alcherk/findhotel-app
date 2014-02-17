69732faeb883cc64a8b4e2988922568829e688f3
Aleksey Cherkasskiy <alcherk@mail.ru>
2014-02-17 17:50:38 +0400

    added iOS project

---
+++ platforms/ios/www/js/findhotel.js
@@ -0,0 +1,18 @@
+document.addEventListener("deviceready", function(){
+  var initialize = function(){
+    var ref = window.open('http://hotels.findhotel.io/?mobile=1&label=android', '_blank', 'location=no,hidden=yes'); 
+    ref.addEventListener('exit', function(event) { 
+      initialize();
+    });
+    ref.addEventListener('loadstop', function(event) { 
+      ref.show();
+    });
+    ref.addEventListener('loadstart', function(event) { 
+      alert('aa');
+      ref.hide();
+      
+    });
+  }
+
+  initialize();
+});
